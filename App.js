5093f585135a0fe42f53a4f9df007320e5a52c7b
mattiasan <mattiasan0408@gmail.com>
2024-12-16 18:24:00 +0100

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './Screens/HomeScreen';
+import AggiungiVotiScreen from './Screens/AggiungiVoti';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const DarkTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#121212',
+    text: '#ffffff',
+    card: '#1F1F1F',
+    border: '#333',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={DarkTheme}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarStyle: {
+            backgroundColor: '#121212',
+            borderTopColor: '#333',
+          },
+          tabBarActiveTintColor: '#76FF03',
+          tabBarInactiveTintColor: '#888',
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Aggiungi Voti') {
+              iconName = 'add-circle';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Tab.Screen name="Aggiungi Voti" component={AggiungiVotiScreen} options={{ headerShown: false }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/*
+ __  __       _   _   _                       
+|  \/  | __ _| |_| |_(_) __ _ ___  __ _ _ __  
+| |\/| |/ _` | __| __| |/ _` / __|/ _` | '_ \ 
+| |  | | (_| | |_| |_| | (_| \__ \ (_| | | | |
+|_|  |_|\__,_|\__|\__|_|\__,_|___/\__,_|_| |_|
+*/
